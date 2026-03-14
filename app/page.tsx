7b632dc8864bf74dcb5e4d46962d3b364ccb5492
vishnu <vishnubharath7890@gmail.com>
2026-03-14 10:48:11 +0530

    add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-export default function Home(){
+export default function Home() {
   return (
     <div style={{padding:40}}>
       <h1>Truck Marketplace</h1>
-      <a href="/login">Go to Login</a>
+      <p>Welcome to the logistics platform</p>
+
+      <a href="/login">Login</a>
+      <br/><br/>
+
+      <a href="/shipper">Shipper Dashboard</a>
+      <br/><br/>
+
+      <a href="/driver">Driver Dashboard</a>
     </div>
   )
 }
