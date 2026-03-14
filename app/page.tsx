8f4efc9095e9620ec39e572b31368b6f2920f506
vishnu <vishnubharath7890@gmail.com>
2026-03-14 10:53:19 +0530

    redirect home to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <div style={{padding:40}}>
-      <h1>Truck Marketplace</h1>
-
-      <p>Logistics bidding platform</p>
-
-      <a href="/login">Login</a>
-      <br/><br/>
-
-      <a href="/shipper">Shipper Dashboard</a>
-      <br/><br/>
-
-      <a href="/driver">Driver Dashboard</a>
-    </div>
-  )
+  redirect("/login")
 }
